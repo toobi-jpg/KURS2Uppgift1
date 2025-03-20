feb494a5090ad846e607c05e33d76d2c086f0131
toobi-jpeg <rex.98@live.se>
2025-03-20 17:47:59 +0100

    Added total price and add menu item function including design changes.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 //Scroll meny logik.....................................................
+const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("header");
+const mobileNav = document.getElementById("mobile-nav-menu");
 let scrollPrev = 0;
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
@@ -23,15 +25,25 @@ window.addEventListener("scroll", () => {
 
   if (scroll <= 290) {
     nav.style.marginTop = "50px";
+    mobileNav.style.marginTop = "50px";
     nav.style.backgroundColor = "transparent";
   } else {
     nav.style.marginTop = "0";
+    mobileNav.style.marginTop = "0";
   }
 
   scrollPrev = scroll;
 });
 //......................................................................
-
+//Hamburger meny logik..................................................
+hamburger.addEventListener("click", () => {
+  if (mobileNav.classList.contains("show-mobile-nav")) {
+    mobileNav.classList.remove("show-mobile-nav");
+  } else {
+    mobileNav.classList.add("show-mobile-nav");
+  }
+});
+//......................................................................
 //Mat meny kategori logik...............................................
 document.addEventListener("DOMContentLoaded", function () {
   const startersBtn = document.getElementById("starters-button");
@@ -92,12 +104,14 @@ const buffetSelect = document.getElementById("buffet-select");
 const carteSelect = document.getElementById("carte-select");
 const carteContainer = document.getElementById("carte-container");
 const buffetContainer = document.getElementById("buffet-container");
+const orderTotalContainer = document.getElementById("order-mode-container");
 
 buffetSelect.addEventListener("click", () => {
   carteSelect.classList.add("apply-shadow-inverse");
   buffetSelect.classList.remove("apply-shadow");
   carteContainer.classList.add("hide-container");
   buffetContainer.classList.remove("hide-container");
+  orderTotalContainer.classList.add("hide-order-shadow");
 });
 
 carteSelect.addEventListener("click", () => {
@@ -105,4 +119,39 @@ carteSelect.addEventListener("click", () => {
   buffetSelect.classList.add("apply-shadow");
   carteContainer.classList.remove("hide-container");
   buffetContainer.classList.add("hide-container");
+  orderTotalContainer.classList.remove("hide-order-shadow");
 });
+//......................................................................
+//Book a table animation hover logik ...................................
+const reservationButton = document.getElementById("reservation-button");
+const tableIcon = document.getElementById("table-icon");
+reservationButton.addEventListener("mouseover", () => {
+  tableIcon.classList.add("table-icon-animation");
+});
+reservationButton.addEventListener("mouseout", () => {
+  tableIcon.classList.remove("table-icon-animation");
+});
+//......................................................................
+//Order total logik ....................................................
+//Kom på i efterhand att kunden kanske vill lägga till flera av samma rätt Hade använt input number istället och sedan multiplicerat mängd med priset.
+const orderTotalText = document.getElementById("order-total");
+const menuCheckbox = document.querySelectorAll(".menu-checkbox");
+
+function total() {
+  let total = 0;
+  menuCheckbox.forEach((box) => {
+    if (box.checked) {
+      total += parseInt(box.dataset.price);
+    }
+  });
+  if (total > 0) {
+    orderTotalText.textContent = `$${total}`;
+  } else {
+    orderTotalText.textContent = "";
+  }
+}
+
+menuCheckbox.forEach((box) => {
+  box.addEventListener("change", total);
+});
+//......................................................................
